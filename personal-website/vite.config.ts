a490b408b9e6d40fe5b631c59fa587ebd20a85e1
Cameron <83986433+Ingratt4@users.noreply.github.com>
2024-08-05 17:28:38 -0400

    new build

--- personal-website/vite.config.ts
+++ personal-website/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "https://github.com/Ingratt4/Portfolio", // Ensure this matches your GitHub Pages project
+  base: "https://github.com/Ingratt4/Portfolio/build", // Ensure this matches your GitHub Pages project
   plugins: [react()],
   build: {
     outDir: "build",
