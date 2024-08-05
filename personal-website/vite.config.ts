891c28d4a474e56d717cf63c432beefb026e100a
Cameron <83986433+Ingratt4@users.noreply.github.com>
2024-08-05 17:58:29 -0400

    hopefully fixed

--- personal-website/vite.config.ts
+++ personal-website/vite.config.ts
@@ -2,17 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/Portfolio", // Ensure this matches your GitHub Pages project
   plugins: [react()],
   build: {
     outDir: "build",
-    rollupOptions: {
-      input: "index.html", // Ensure the correct entry point
-      output: {
-        assetFileNames: "assets/[name].[hash][extname]",
-        chunkFileNames: "assets/[name].[hash].js",
-        entryFileNames: "assets/[name].[hash].js",
-      },
-    },
   },
+  base: "/Portfolio/", // Ensure this matches your GitHub repository name
 });
